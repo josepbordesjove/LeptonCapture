e57c7b216e4e2b53e0426209a07bb42949bd9898
Josep Bordes Jové <josepbordesjove@gmail.com>
2016-07-18 12:25:56 +0200

    Delete files, in order to add the new ones

--- LeptonCapture/include/menu.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//Shows the available options that the user can choose
-char mainMenu();
--- Lepton_SDK/Debug/src_crc16fast.c.o.d
+++
@@ -1,3 +0,0 @@
-Debug/src_crc16fast.c.o: src/crc16fast.c include/crc16.h
-
-include/crc16.h:
